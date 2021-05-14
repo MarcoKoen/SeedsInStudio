b2d6741facc395f22c3e9c4235044b21a5b0d943
Carlson-Lee <carlsonzhong@gmail.com>
2021-05-14 14:10:03 +1200

    height

--- seeds/Program.cs
+++ seeds/Program.cs
@@ -6,7 +6,25 @@ namespace seeds
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Height;
+
+
+            Console.WriteLine("Please enter your height.");
+            Height = Convert.ToInt32(Console.ReadLine());
+
+            if (Height > 170)
+            {
+                Console.WriteLine("Hello, daddy long leg.");
+            }
+            else if (Height == 183)
+            {
+                Console.WriteLine("Yooo Caleb,");
+            }
+            else
+            {
+                Console.WriteLine("Supp tiny.");
+            }
+            Console.ReadLine();
         }
     }
 }
