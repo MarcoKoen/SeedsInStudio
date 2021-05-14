c424eb8b1af166083319295d46b2dc05686f2947
MarcoKoen <neokocram@gmail.com>
2021-05-14 14:00:16 +1200

    Added a questionaire

--- seeds/Program.cs
+++ seeds/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace seeds
 {
@@ -6,7 +7,31 @@ namespace seeds
     {
         static void Main(string[] args)
         {
+            int count = 0, joy = 0, carlson = 0;
+            Console.WriteLine("Who is your favourite Studio lecturer");
+            joy = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(joy);
+            Thread.Sleep(1000);
+
+            if (joy == 1)
+            {
+                Console.WriteLine("That is absolutely correct");
+            }
+
+            if (joy <= 0) {
+                Console.WriteLine("that is an absolute crime");
+                    }
+
+            Console.ReadLine();
+            count++;
+            carlson = -10;
+
+
+
             Console.WriteLine("Hello World!");
+
+
+
         }
     }
 }
